9a43906539e7fd0515d60f6ac2b9d3efadabf2d4
kjh-busan <picsmash7@gmail.com>
2023-03-26 23:58:57 +0900

    feat: React.memo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/UI/Button/DEMO/DemoOutput";
 import "./App.css";
@@ -6,16 +6,16 @@ import "./App.css";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleShowHandler = () => {
+  const toggleShowHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   console.log("App is Running");
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph} />
+      <DemoOutput />
       <Button onClick={toggleShowHandler}>Toggle Button</Button>
     </div>
   );
---
+++ src/components/UI/Button/DEMO/MyParagraph.js
@@ -0,0 +1,7 @@
+const MyParagraph = (props) => {
+  console.log("My Paragraph");
+
+  return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
